0f8ecd3eb31cd3ed1d41c66ea4f8247012cd2836
dcdholder <dcdholder@gmail.com>
2016-06-15 22:40:30 -0400

    P142 still not working, giving up for now

--- P142/src/dcdholder/projectEuler/p142/ProjectEuler142.java
+++ P142/src/dcdholder/projectEuler/p142/ProjectEuler142.java
@@ -1,31 +1,73 @@
 package dcdholder.projectEuler.p142;
 
-class ProjectEulerP142 {
+import java.util.*;
+
+public class ProjetEuler142 {
 	static int maxTriple = 1000;
 
 	public static void main(String[] args) {
-		Set<Integer[3]> abcValuesSet = getAbcValuesSet();
-		filterAbcValuesSet(abcValuesSet);
-		int minSum = getMinXyzSumFromAbcValues(abcValuesSet);
+		Set<Integer[]> abcValuesSet         = getAbcValuesSet();
+		Set<Integer[]> filteredAbcValuesSet = filterAbcValuesSet(abcValuesSet);
+		int minSum = getMinXyzSumFromAbcValues(filteredAbcValuesSet);
 		
-		System.out.println("Examined " + getAbcValues.size() + " ABC combinations");
-		System.out.println("The minimum sum found in the search was" + minSum);
+		System.out.println("Examined " + filteredAbcValuesSet.size() + " ABC combinations");
+		System.out.println("The minimum sum found in the search was " + minSum);
 	}
 
-	private static Set<Integer[3]> getAbcValuesSet() {
-		Map<Integer,HashSet<Integer[2]>> coupleMap = PythagoreanTriples.pythagoreanCouplesUpToNMapped(maxTriple);
-		HashSet<Integer[3]> abcValuesSet = new HashSet<Integer[3]>();
+	private static Set<Integer[]> getAbcValuesSet() {
+		Map<Integer,HashSet<Integer[]>> coupleMap = PythagoreanTriple.pythagoreanCouplesUpToNMapped(maxTriple);
+		HashSet<Integer[]> abcValuesSet = new HashSet<Integer[]>();
 
+		/*
+		if(coupleMap.containsKey(34)) {
+			System.out.println("Shit");
+			for(Integer[] blach : coupleMap.get(34)) {
+				for(int i=0;i<2;i++) {
+					System.out.println(blach[i]);
+				}
+			}
+		}
+		*/
+		
+		/*
 		for(Integer coupleKey : coupleMap.keySet()) {
-			for(int[2] couple : coupleMap.get(coupleKey)) {
-				int[] abcValues = new int[3];
+			for(Integer[] couple : coupleMap.get(coupleKey)) {
+				Integer[] abcValues = new Integer[3];
 
 				abcValues[0] = couple[0];
 				abcValues[1] = couple[1];
+				if(coupleMap.containsKey(couple[1])) {
+					for(Integer[] coupleB : coupleMap.get(couple[1])) {
+						abcValues[2] = coupleB[1];
+						abcValuesSet.add(abcValues.clone()); //create a copy!!!!!!!!!!!
+					}
+				}
+			}
+		}
+		*/
+		for(Integer a : coupleMap.keySet()) {
+			HashSet<Integer> aToB = new HashSet<Integer>();
+			for(Integer[] aCouple : coupleMap.get(a)) {
+				aToB.add(aCouple[1]);
+			}
+			for(Integer b : aToB) {
+				HashSet<Integer> bToC = new HashSet<Integer>();
+				if(coupleMap.containsKey(b)) {
+					for(Integer[] bCouple : coupleMap.get(b)) {
+						bToC.add(bCouple[1]);
+					}
+				}
+				for(Integer c : bToC) {
+					if(a==34 && b==30) {System.out.println(c);}
+					if(aToB.contains(c)) {
+						Integer[] abcValues = new Integer[3];
 
-				for(int[2] couple : coupleMap.get(coupleKey)) {
-					abcValues[2] = couple[1];
-					abcValuesSet.add(abcValues);
+						abcValues[0] = a;
+						abcValues[1] = b;
+						abcValues[2] = c;
+						
+						abcValuesSet.add(abcValues);
+					}
 				}
 			}
 		}
@@ -34,33 +76,42 @@ class ProjectEulerP142 {
 	}
 
 	//check the set of generated values arrays for the various tests
-	private static void filterAbcValues(Set<Integer[3]> abcValuesSet) {
-		for(int[3] abcValues : abcValuesSet) {
-			if(!abcValues[1]*abcValues[1]+abcValues[2]*abcValues[2]>abcValues[0]*abcValues[0]) {
-				abcValues.remove(abcValues); //fails the b^2+c^2>a^2 test
+	private static Set<Integer[]> filterAbcValuesSet(Set<Integer[]> abcValuesSet) {
+		Set<Integer[]> filteredAbcValuesSet = new HashSet<Integer[]>();
+		
+		for(Integer[] abcValues : abcValuesSet) {
+			boolean addAbc = true;
+			if((abcValues[1]*abcValues[1]+abcValues[2]*abcValues[2])<(abcValues[0]*abcValues[0])) {
+				addAbc = false; //fails the b^2+c^2>a^2 test
 			}
-			if(abcValues[1]>abcValues[0] || !abcValues[2]>abcValues[0]) {
-				abcValues.remove(abcValues); //fails the a>b, a>c test
+			if(abcValues[1]>=abcValues[0] || abcValues[2]>=abcValues[0]) {
+				addAbc = false; //fails the a>b, a>c test
 			}
-			if(abcValues[2]>abcValues[1]) {
-				abcValues.remove(abcValues); //fails the b>c test
+			if(abcValues[2]>=abcValues[1]) {
+				addAbc = false; //fails the b>c test
 			}
-			if(abcValues[0]*abcValues[0]+abcValues[1]*abcValues[1]+abcValues[2]*abcValues[2]%2!=0) {
-				abcValues.remove(abcValues); //sum of squares is not divisible by 2
+			if((abcValues[0]*abcValues[0]+abcValues[1]*abcValues[1]+abcValues[2]*abcValues[2])%2!=0) {
+				addAbc = false; //sum of squares is not divisible by 2
+			}
+			if(addAbc) {
+				filteredAbcValuesSet.add(abcValues);
 			}
 		}
+		
+		return filteredAbcValuesSet;
 	}
 
-	private static int getMinXyzSumFromAbcValues(Set<Integer[3]> filteredAbcValuesSet) {
-		int smallestSum;
+	private static int getMinXyzSumFromAbcValues(Set<Integer[]> filteredAbcValuesSet) {
+		int smallestSum=0;
 
 		boolean firstIteration = true;
-		for(int[3] abcValues : filteredAbcValuesSet) {
+		for(Integer[] abcValues : filteredAbcValuesSet) {
 			//x+y+z = a^2+b^2+c^2 where x+y=a^2, x+z=b^2, y+z=c^2, x>y>z>0
 			int currentSum = (abcValues[0]*abcValues[0]+abcValues[1]*abcValues[1]+abcValues[2]*abcValues[2])/2;
 			
 			if(currentSum<smallestSum || firstIteration) {
 				smallestSum = currentSum;
+				System.out.println(abcValues[0] + "," + abcValues[1] + "," + abcValues[2]);
 			}
 			firstIteration = false;
 		}
--- P142/src/dcdholder/projectEuler/p142/PythagoreanTriple.java
+++ P142/src/dcdholder/projectEuler/p142/PythagoreanTriple.java
@@ -1,6 +1,8 @@
 package dcdholder.projectEuler.p142;
 
-class PythagoreanTriple {
+import java.util.*;
+
+public class PythagoreanTriple {
 	int largestSide;
 	int middleSide;
 	int smallestSide;
@@ -12,13 +14,14 @@ class PythagoreanTriple {
 	static public Set<PythagoreanTriple> generatePrimitiveTriplesUpToN(int maxValue) {
 		Set<PythagoreanTriple> primitiveTriples = new HashSet<PythagoreanTriple>();
 
+		infiniteLoop:
 		for(int m=2; ; m++) {
 			for(int n=1; n<m; n++) {
 				if(m*m+n*n>maxValue) {
-					if(n=1) {
-						break 2; //kill the infinite loop this is nested in if you're already blowing the limit by n=1
+					if(n==1) {
+						break infiniteLoop; //kill the infinite loop this is nested in
 					} else {
-						break 1;
+						break;
 					}
 				} else {
 					primitiveTriples.add(new PythagoreanTriple(new int[] {m*m-n*n,2*m*n,m*m+n*n})); //Euclid's formula
@@ -34,9 +37,9 @@ class PythagoreanTriple {
 
 		for(PythagoreanTriple primitiveTriple : generatePrimitiveTriplesUpToN(maxValue)) {			
 			for(int i=2; ; i++) {
-				int newSmallestSide = primitiveTriple.getSmallestSide() * k;
-				int newMiddleSide   = primitiveTriple.getMiddleSide()   * k;
-				int newLargestSide  = primitiveTriple.getLargestSide()  * k;
+				int newSmallestSide = primitiveTriple.getSmallestSide() * i;
+				int newMiddleSide   = primitiveTriple.getMiddleSide()   * i;
+				int newLargestSide  = primitiveTriple.getLargestSide()  * i;
 
 				if(newLargestSide>maxValue) {
 					break;
@@ -51,27 +54,28 @@ class PythagoreanTriple {
 
 	//only two elements in the triple are needed to identify it (the other can be derived) - two such pairs exist for each couple
 	//largest value is at index 0, smaller value at index 1
-	static private Set<Integer[2]> pythagoreanCouplesUpToN(int maxValue) {
-		Set<Integer[2]> pythagoreanCouples = new HashSet<Integer[2]>();
+	static private Set<Integer[]> pythagoreanCouplesUpToN(int maxValue) {
+		Set<Integer[]> pythagoreanCouples = new HashSet<Integer[]>();
 
 		for(PythagoreanTriple triple : generateAllTriplesUpToN(maxValue)) {
-			pythagoreanCouples.add(new int[2] {triple.getLargestSide(),triple.getMiddleSide()});
-			pythagoreanCouples.add(new int[2] {triple.getLargestSide(),triple.getSmallestSide()});
+			pythagoreanCouples.add(new Integer[] {triple.getLargestSide(),triple.getMiddleSide()});
+			pythagoreanCouples.add(new Integer[] {triple.getLargestSide(),triple.getSmallestSide()});
 		}
 
 		return pythagoreanCouples;
 	}
 
 	//multiple triples can share the same largest value
-	static public Map<Integer,HashSet<Integer[2]>> pythagoreanCouplesUpToNMapped(int maxValue) {
-		Map<Integer,HashSet<Integer[2]>> coupleMap = new HashMap<Integer,Integer[2]>();
+	static public Map<Integer,HashSet<Integer[]>> pythagoreanCouplesUpToNMapped(int maxValue) {
+		Map<Integer,HashSet<Integer[]>> coupleMap = new HashMap<Integer,HashSet<Integer[]>>();
 
-		for(int[2] couple : pythagoreanCouplesUpToN(maxValue)) {
+		for(Integer[] couple : pythagoreanCouplesUpToN(maxValue)) {
 			if(coupleMap.keySet().contains(couple[0])) {
-				HashSet<int[2]> matchingCouples = coupleMap.get(couple[0]);
+				HashSet<Integer[]> matchingCouples = coupleMap.get(couple[0]);
 				matchingCouples.add(couple);
 			} else {
-				HashSet<int[2]> matchingCouples = new HashSet<int[2]>();
+				HashSet<Integer[]> matchingCouples = new HashSet<Integer[]>();
+				matchingCouples.add(couple);
 				coupleMap.put(couple[0],matchingCouples);
 			}
 		}
@@ -84,7 +88,7 @@ class PythagoreanTriple {
 			return false;
 		} else {
 			PythagoreanTriple tripleO = (PythagoreanTriple)o;
-			if(largestSide.o!=this.largestSide || middleSide.o!=this.middleSide || smallestSide.o!=this.smallestSide) {
+			if(tripleO.largestSide!=this.largestSide || tripleO.middleSide!=this.middleSide || tripleO.smallestSide!=this.smallestSide) {
 				return false;
 			} else {
 				return true;
@@ -95,33 +99,47 @@ class PythagoreanTriple {
 	public int hashCode() {
 		return largestSide*11 + middleSide*13 + smallestSide*17;
 	}
+	
+	public String toString() {
+		return "{" + largestSide + "," + middleSide + "," + smallestSide + "}";
+	}
 
 	PythagoreanTriple(int[] sides) {
 		if(sides.length!=3) {
 			throw new IllegalArgumentException("Incorrect number of entries in triple: " + sides.length);
 		}
 
-		largestSide  = sides[0];
-		middleSide   = sides[0];
-		smallestSide = sides[0];
+		largestSide  = 0;
+		middleSide   = 0;
+		smallestSide = 0;
 		for(int i=0;i<3;i++) { //three entry sort lol
 			if(sides[i]<1) {
 				throw new IllegalArgumentException("One of the sides is shorter than 1 - its length is: " + sides[i]);
 			}
 
-			if(sides[i]>largestSide) {
+			if(sides[i]>largestSide || largestSide==0) {
 				largestSide = sides[i];
 			}
-			if(sides[i]<smallestSide) {
-				largestSide = sides[i];
-			}
-			if(sides[i]<largestSide && sides[i]>smallestSide) {
-				middleSide  = sides[i];
+			if(sides[i]<smallestSide || smallestSide==0) {
+				smallestSide = sides[i];
 			}
 		}
+		//very ugly hack
+		boolean middleFound=false;
+		for(int i=0;i<3;i++) {
+			if(sides[i]!=largestSide && sides[i]!=smallestSide) {
+				middleSide  = sides[i];
+				middleFound = true;
+			}
+		}
+		if(!middleFound) {
+			middleSide = smallestSide;
+		}
 
+		System.out.println(this.toString());
 		if(smallestSide*smallestSide+middleSide*middleSide!=largestSide*largestSide) {
-			throw new IllegalArgumentException("Not a valid triple");
+			throw new IllegalArgumentException("Not a valid triple: {" + largestSide + "," + middleSide + "," + smallestSide + "}");
 		}
 	}
 }
+
